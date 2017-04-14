84d9b10f1810ecb862e44cd22a51185c187d6fa0
Aston5128 <2338733304@qq.com>
2017-04-14 13:42:10 +0800

    Update DouTuSpider.py
    Append threading

--- DouTuSpider.py
+++ DouTuSpider.py
@@ -1,36 +1,62 @@
 # 斗图网表情包 Spider
+# 在原有基础上加上一个多线程：threading 库是 Python 原生多线程处理与控制库
 
-import os, re, requests
-from Download import dl
+import os
+import re
+import requests
+import threading
 from bs4 import BeautifulSoup
+from Download import dl
 
 
 class DouTu:
     def __init__(self):
         self.start_url = 'http://www.doutula.com/'
+        os.mkdir('/Users/yton/Documents/斗图')
+        os.chdir('/Users/yton/Documents/斗图')
 
-    def storeImgs(self, href, dir_name):
-        os.mkdir(dir_name)
+    @staticmethod
+    def getImgHref(item):
+        img_href_n = item.find('img')
+        if img_href_n:
+            return {
+                'img_name': img_href_n['alt'],
+                'img_href': re.findall("this.src='(.*?)'", img_href_n['onerror'])[0]
+            }
+
+    def getImgs(self, href, dir_name):
+        os.mkdir('/Users/yton/Documents/斗图/' + dir_name)
+        os.chdir('/Users/yton/Documents/斗图/' + dir_name)
         soup = BeautifulSoup(dl.GetHtml(href), 'lxml').find_all('div', class_='artile_des')
-        for item in soup:
-            img_href_n = item.find('img')
-            if img_href_n:
-                img_name = img_href_n['alt']
-                img_href = re.findall("this.src='(.*?)'", img_href_n['onerror'])[0]
-                print('[正在抓取]。。。' + img_name + ' ' + img_href)
-                # requests.get().text 返回的 Unicode 数据类型，用于获取源码和文本
-                # requests.get().content 返回的是二进制数据类型，用于获取文件和图片
-                img_content = requests.get(img_href).content
-                img_output = open(dir_name + os.sep + img_name + '.jpg', 'ab')
-                img_output.write(img_content)
-                img_output.close()
+        img_href_list = [self.getImgHref(item) for item in soup]
+        img_href_list.pop()
+        self.startSaveImgs(img_href_list)
+
+    def startSaveImgs(self, img_href_list):
+        for detail_img in img_href_list:
+            img_href = detail_img['img_href']
+            img_name = detail_img['img_name']
+            # threanding 是多线程处理控制，Thread 是多线程，target 是在线程启动后执行的，args 表示调用 target 的参数列表
+            th = threading.Thread(target=self.saveDetailImg, args=(img_name, img_href))
+            # 启动线程
+            th.start()
+
+    @staticmethod
+    def saveDetailImg(img_name, img_href):
+        print('[正在抓取]。。。' + img_name + ' ' + img_href)
+        # requests.get().text 返回的 Unicode 数据类型，用于获取源码和文本
+        # requests.get().content 返回的是二进制数据类型，用于获取文件和图片
+        img_content = requests.get(img_href).content
+        img_output = open(img_name + '.jpg', 'wb')
+        img_output.write(img_content)
+        img_output.close()
 
     def getDetail(self, soup):
         for item in soup:
             href = item['href']
             dir_name = item.find('h4').text
             print('[正在抓取]。。。' + dir_name)
-            self.storeImgs(href, dir_name)
+            self.getImgs(href, dir_name)
 
     def get(self):
         html = dl.GetHtml(self.start_url)
@@ -44,3 +70,5 @@ class DouTu:
 
 if __name__ == '__main__':
     DouTu().get()
+else:
+    doutu = DouTu()
